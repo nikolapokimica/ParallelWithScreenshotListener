7bf81bf7296495577e03739c8b3db0975d3db5f3
Nikola Pokimica <nikola.pokimica@gmail.com>
2021-10-14 13:28:47 +0200

    Added project

--- src/test/java/tests_with_login/BaseTestWithLogin.java
+++ src/test/java/tests_with_login/BaseTestWithLogin.java
@@ -1,10 +1,14 @@
 package tests_with_login;
 
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.BeforeMethod;
 import pages.LoginPage;
 import pages.VerifyPage;
 import tests.BaseTest;
 import utilities.PropertyManager;
+
+import java.util.concurrent.TimeUnit;
 
 public abstract class BaseTestWithLogin extends BaseTest {
 
@@ -13,7 +17,11 @@ public abstract class BaseTestWithLogin extends BaseTest {
     @Override
     @BeforeMethod
     public void setup() {
-        super.setup();
+        System.setProperty("webdriver.chrome.driver", PropertyManager.getInstance().getDriverPath());
+
+        driver = new ChromeDriver(new ChromeOptions().addArguments("--disable-notifications"));
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
 
         new LoginPage(driver)
                 .openHomePage(PropertyManager.getInstance().getUrl())
--- src/test/java/tests_with_login/BuyOneItemFromHomePageTest.java
+++ src/test/java/tests_with_login/BuyOneItemFromHomePageTest.java
@@ -14,7 +14,7 @@ public class BuyOneItemFromHomePageTest extends BaseTestWithLogin {
                 .selectArticleAndAddToCart();
 
         try {
-            new VerifyPage(driver).verifyOrderConfirmation("Your order on My Store is complete.");
+            verifyPage.verifyOrderConfirmation("Your order on My Store is complete.");
             System.out.println("BuyOneItemFromHomePageTest: Item is ordered");
         } catch (Exception e) {
             Assert.fail("Order wasn't completed");
